704fe42b2e730625f9bc00d7df8e830df23a6cce
SorenA <s.dev@lysdal.org>
2020-06-24 23:09:29 +0200

    Add sample application.

---
+++ samples/Sample.MetaFieldService/Data/BogusMetaFieldFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+using LightOps.Commerce.Services.MetaField.Api.Models;
+
+namespace Sample.MetaFieldService.Data
+{
+    public class BogusMetaFieldFactory
+    {
+        public int? Seed { get; set; }
+
+        public IList<IMetaField> MetaFields { get; internal set; } = new List<IMetaField>();
+
+        public void Generate()
+        {
+            if (Seed.HasValue)
+            {
+                Randomizer.Seed = new Random(Seed.Value);
+            }
+
+            // TODO: Generate data
+        }
+    }
+}
